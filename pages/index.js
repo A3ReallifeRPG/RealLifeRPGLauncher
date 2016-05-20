d131e12a474359b9540b33dd682357fe7315176a
Greeny <Green4ever999@web.de>
2016-05-20 14:25:56 +0200

    now ?

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const app = require('electron').remote
 const shell = require('electron').shell;
 const storage = require('electron-json-storage')
 const Winreg = require('winreg')
-const dialog = require('electron').remote
+const {dialog} = require('electron').remote
 const {ipcRenderer} = require('electron');
 
 getLauncherNotification(showNotf);
