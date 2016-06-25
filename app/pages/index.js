7af4c71bf670de49b4004c5cfb4617e5b7129c54
Kaibu <Whocaresaboutgreeny@web.de>
2016-06-25 22:14:03 +0200

    Update notification

--- app/pages/index.js
+++ app/pages/index.js
@@ -450,6 +450,11 @@ function resetWinProgress() {
     ipcRenderer.send('winprogress-change', args);
 }
 
+ipcRenderer.on('update-downloaded', (event, arg) => {
+    notifyWin('Update heruntergeladen', 'Bitte neustarten','ic_done_white_36dp_2x.png');
+    console.log(args);
+};
+
 function notifyWin(title, text, icon) {
     notifier.notify({
         title: title,
