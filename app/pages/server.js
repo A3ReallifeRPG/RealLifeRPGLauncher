2c42279db5b8c8429400bb7f2219f2fd85c92f20
vabene1111 <vabene1234@googlemail.com>
2016-06-30 16:01:20 +0200

    server names

--- app/pages/server.js
+++ app/pages/server.js
@@ -1,12 +1,12 @@
 getServerInfo(getServerCallback);
 
 function getServerCallback(jsObj) {
-    if(jsObj.length > 0){
+    if (jsObj.length > 0) {
         defaultServer = jsObj[0].Id;
         for (var i = 0; i < jsObj.length; i++) {
-            insertServerTab(jsObj[i],i);
+            insertServerTab(jsObj[i], i);
         };
-        $(function(){
+        $(function() {
             $("#tabcontroller").tabcontrol();
         });
         $('#tabcontroller').css({
@@ -55,64 +55,113 @@ function getServerCallback(jsObj) {
 <li><a href="#mod1">Server 1</a></li>
 */
 
-function setPlayerList(serverId,playerList){
-    debugger;
+function insertServerTab(serverObj, index) {
+    if (serverObj.Slots != 0) {
+        //build server info
+        var wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'frame');
+        wrapper.setAttribute('id', ('server_' + serverObj.Id));
+
+        var row = document.createElement('div');
+        row.setAttribute('class', 'row');
+
+        var col4 = document.createElement('div');
+        col4.setAttribute('class', 'col-md-4');
+        col4.setAttribute('style', 'height:100%');
+
+        var charDiv = document.createElement('div');
+        charDiv.setAttribute('id', 'char_server_' + serverObj.Id);
+        charDiv.setAttribute('style', 'min-width: 20%; height: 200px;');
+
+        var br = document.createElement("br");
+        var br2 = document.createElement("br");
+        var txtPlayers = document.createTextNode("Aktuelle Spieler: " + serverObj.Players + "/" + serverObj.Slots);
+        var txtIp = document.createTextNode("IP: " + serverObj.IpAddress + " Port: " + serverObj.Port);
+
+        var btnJoin = document.createElement('button');
+        btnJoin.setAttribute('class', 'button loading-pulse lighten success');
+        btnJoin.setAttribute('style', 'margin-top:20px');
+        btnJoin.setAttribute('id', ('btn_start_' + serverObj.Id));
+        btnJoin.setAttribute('onClick', ('joinServer(' + serverObj.Id + ')'));
+        var txtBtnJoin = document.createTextNode(" Server beitreten");
+        btnJoin.appendChild(txtBtnJoin);
+
+        col4.appendChild(charDiv);
+        col4.appendChild(txtPlayers);
+        col4.appendChild(br);
+        col4.appendChild(txtIp);
+        col4.appendChild(br2);
+        col4.appendChild(btnJoin);
+        row.appendChild(col4);
+
+        //build player list
+        var col8 = document.createElement('div');
+        col8.setAttribute('class', 'col-md-8');
+
+        var tbWrapper = document.createElement('div');
+        tbWrapper.setAttribute('id', 'table-wrapper');
+
+        var tbScroll = document.createElement('div');
+        tbScroll.setAttribute('id', 'table-scroll');
+
+        var tbl = document.createElement('table');
+        tbl.setAttribute('class','table');
+
+
+        var tblHead = document.createElement('thead');
+        var tblHeadRow = document.createElement('tr');
+        var tblHeadRowH = document.createElement('th');
+        var tblHeadRowHSpan = document.createElement('span');
+        var txtTblHead = document.createTextNode("Name");
+
+        tblHeadRowHSpan.appendChild(txtTblHead);
+        tblHeadRowH.appendChild(tblHeadRowHSpan);;
+        tblHeadRow.appendChild(tblHeadRowH);
+        tblHead.appendChild(tblHeadRow);
+
+        var tblBody = document.createElement('tbody');
+        tblBody.setAttribute('id',('tb_list_' + serverObj.Id));
+
+        tbl.appendChild(tblHead);
+        tbl.appendChild(tblBody);
+
+        tbScroll.appendChild(tbl);
+        tbWrapper.appendChild(tbScroll);
+        col8.appendChild(tbWrapper);
+
+        row.appendChild(col8);
+
+        wrapper.appendChild(row);
+        //attach to host
+        document.getElementById('server_tabs').appendChild(wrapper);
+        var txtTab = document.createTextNode("Server " + (index + 1));
+        var li = document.createElement('li');
+        var liA = document.createElement('a');
+        liA.setAttribute('href', ('#server_' + serverObj.Id));
+        liA.appendChild(txtTab);
+        li.appendChild(liA);
+
+        document.getElementById('server_tabHost').appendChild(li);
+
+        loadHighCharts(("char_server_" + serverObj.Id), serverObj.Civilians, serverObj.Cops, serverObj.Medics, serverObj.Adac);
+    };
 }
 
-function insertServerTab(serverObj,index) {
-  if(serverObj.Slots != 0) {
-    var wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'frame');
-    wrapper.setAttribute('id', ('server_' + serverObj.Id));
+function setPlayerList(serverId, playerList) {
+    var tBody = document.getElementById('tb_list_' + serverId);
+    for (i = 0; i < playerList.length; i++) {
 
-    var row = document.createElement('div');
-    row.setAttribute('class', 'row');
+        var tbRow = document.createElement('tr');
+        var tbCol = document.createElement('td');
+        var txtCol = document.createTextNode(playerList[i]);
 
-    var col4 = document.createElement('div');
-    col4.setAttribute('class', 'col-md-4');
-    col4.setAttribute('style', 'height:100%');
-
-    var charDiv = document.createElement('div');
-    charDiv.setAttribute('id', 'char_server_' + serverObj.Id);
-    charDiv.setAttribute('style', 'min-width: 20%; height: 200px;');
-
-    var br = document.createElement("br");
-    var br2 = document.createElement("br");
-    var txtPlayers = document.createTextNode("Aktuelle Spieler: " + serverObj.Players + "/" + serverObj.Slots);
-    var txtIp = document.createTextNode("IP: " + serverObj.IpAddress + " Port: " + serverObj.Port);
-
-    var btnJoin = document.createElement('button');
-    btnJoin.setAttribute('class', 'button loading-pulse lighten success');
-    btnJoin.setAttribute('style', 'margin-top:20px');
-    btnJoin.setAttribute('id', ('btn_start_' + serverObj.Id));
-    btnJoin.setAttribute('onClick', ('joinServer(' + serverObj.Id + ')'));
-    var txtBtnJoin = document.createTextNode(" Server beitreten");
-    btnJoin.appendChild(txtBtnJoin);
-
-    col4.appendChild(charDiv);
-    col4.appendChild(txtPlayers);
-    col4.appendChild(br);
-    col4.appendChild(txtIp);
-    col4.appendChild(br2);
-    col4.appendChild(btnJoin);
-    row.appendChild(col4);
-    wrapper.appendChild(row);
-
-    document.getElementById('server_tabs').appendChild(wrapper);
-    var txtTab = document.createTextNode("Server " + (index + 1));
-    var li = document.createElement('li');
-    var liA = document.createElement('a');
-    liA.setAttribute('href',('#server_' + serverObj.Id));
-    liA.appendChild(txtTab);
-    li.appendChild(liA);
-
-    document.getElementById('server_tabHost').appendChild(li);
-
-    loadHighCharts(("char_server_" + serverObj.Id),serverObj.Civilians, serverObj.Cops, serverObj.Medics, serverObj.Adac);
-  };
+        tbCol.appendChild(txtCol);
+        tbRow.appendChild(tbCol);
+        tBody.appendChild(tbRow);
+    }
 }
 
-function loadHighCharts(chart,civ, cop, med, adac) {
+function loadHighCharts(chart, civ, cop, med, adac) {
     var Highcharts = require('highcharts');
     $(function() {
         $('#' + chart).highcharts({
